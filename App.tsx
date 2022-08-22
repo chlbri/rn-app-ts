aa8efb4e71ee727fcf9fd6a1d027ae8f761d4223
chlbri <bri_lvi@icloud.com>
2022-08-22 03:05:59 +0000

    app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { FC } from 'react';
 import RouteStackRoute from './src/routes/RootStack';
 
-export default () => {
+const App: FC = () => {
   return (
     <NavigationContainer>
       <RouteStackRoute />
     </NavigationContainer>
   );
 };
+
+export default App;
